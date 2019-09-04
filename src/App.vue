b6cf6ab71d490daff1fbefb2ef4392efbc64ee18
Andrey Radantsev <avradantsev@yandex.ru>
2019-09-04 16:35:44 +0300

    Проект подготовлен и собран каркас приложения

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,36 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <Header/>
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+
+    <v-footer
+      app
+      color="#BC104E"
+      class="white--text"
+    >
+      <span>Cash.Счетчик</span>
+      <div class="flex-grow-1"></div>
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import Header from './components/Header'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  },
+  data: () => ({
+  })
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <v-navigation-drawer
+      v-model="drawerRight"
+      app
+      clipped
+      right
+    >
+      <v-list dense>
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon>mdi-exit-to-app</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Войти</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      app
+      clipped-right
+    >
+      <v-toolbar-title>Cash.Счетчик</v-toolbar-title>
+      <div class="flex-grow-1"></div>
+      <v-app-bar-nav-icon @click.stop="drawerRight = !drawerRight"></v-app-bar-nav-icon>
+    </v-app-bar>
+  </header>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawerRight: null
+  })
+}
+</script>
